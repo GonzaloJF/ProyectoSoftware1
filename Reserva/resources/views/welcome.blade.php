cfebe95e4840dd10e636abb971f8f61a1fd384e5
Tomasespinozagarrido <tomasmate.teg@gmail.com>
2020-07-09 19:59:16 -0400

    Update welcome.blade.php

--- Reserva/resources/views/welcome.blade.php
+++ Reserva/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Reserva de Laboratorios</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                Reserva de Laboratorios
                 </div>
 
                 <div class="links">
