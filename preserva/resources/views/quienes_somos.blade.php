88896c70f5991546fca605ae72384972ec20acd5
Gonza17 <gonza.a.jaques17@gmail.com>
2020-07-14 05:16:50 -0400

    asdasd

--- preserva/resources/views/quienes_somos.blade.php
+++ preserva/resources/views/quienes_somos.blade.php
@@ -1,63 +1,44 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.0.1">
-    <title>¿Quienes Somos?</title>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/cover/">
-
-    <!-- Bootstrap core CSS -->
-<link href="/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
-<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
-<meta name="theme-color" content="#563d7c">
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
-  </head>
-  <body class="text-center">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header class="masthead mb-auto">
-    <div class="inner">
-      <nav class="nav nav-masthead justify-content-center">
-        <a class="nav-link active" href="/">Inicio</a>
-      </nav>
-    </div>
-  </header>
-
-  <main role="main" class="inner cover">
-    <h1 class="cover-heading">Somos Un Grupo de Estudiantes</h1>
-    <p class="lead">Creamos esta plataforma para poder reservar los distintos Laboratorios que se pueden ocupar dentro de nuestra universidad</p>
-  </main>
-
+        <title>¿Quienes somos?</title>
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+    
 </div>
-</body>
+    <body>
+    <nav id="navbar-example2" class="navbar navbar-light bg-light">
+    <div class="container">
+    <div class="row">      
+      <a class="navbar-brand" href="#">¿Quienes somos?</a>
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login">Login</a>
+            </li>
+            <li class="nav-iten">
+              <a class="nav-link" href="register">registrarse</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <header class="container-fluid">
+      <div class="row" style="height: 680px; background-color:#008080">
+        <div class="col12 align-self-center text-center">
+          <img src="{{asset('imagenes/imagen1.png')}}" class="img-fluid" width="600px" alt="">
+        </div>
+        <div class="col12">        
+          <h1>Somos Un Grupo de Estudiantes de la Universidad Catolica Del Maule</h1>
+          <p>creamos un proyecto de ingenieria en Software 1, con el que se hara una pagina de registros de reservas de los distintos laboratorios que hay en la universidad.</p>
+        </div>
+      </div>
+    </header>
 </html>
