ec142d95ff44458b4a2501864112a2fa3a5e3c3e
Tomasespinozagarrido <tomasmate.teg@gmail.com>
2020-07-14 00:56:38 -0400

    Update login.blade.php

--- preserva/resources/views/auth/login.blade.php
+++ preserva/resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         @csrf
 
                         <div class="form-group row ">
-                            <label for="login" class="col-md-4 col-form-label text-md-right">{{ __('rut o correo') }}</label>
+                            <label for="login" class="col-md-4 col-form-label text-md-right">{{ __('rut') }}</label>
 
                             <div class="col-md-6">
                                 <input id="login" type="text" class="form-control  @error('login') is-invalid @enderror " name="login" value="{{ old('username') }}" required  autofocus>
