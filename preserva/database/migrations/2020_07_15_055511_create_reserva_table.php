81f5fa55a298bfb3fc11af95484f37c4a19eeadf
Tomasespinozagarrido <tomasmate.teg@gmail.com>
2020-07-15 02:04:51 -0400

    asd

--- preserva/database/migrations/2020_07_15_055511_create_reserva_table.php
+++ preserva/database/migrations/2020_07_15_055511_create_reserva_table.php
@@ -13,19 +13,17 @@ class CreateReservaTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reserva', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
-            $table->string('apellido');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('tipo_usuario');
-            $table->string('estado');
-            $table->string('ciudad');
-            $table->rememberToken();
+            $table->string('cod_lab');
+            $table->date('fecha');
+            $table->integer('bloque');
+            $table->integer('capacidad');
             $table->timestamps();
+
+            
         });
     }
 
@@ -36,6 +34,6 @@ class CreateReservaTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reserva');
     }
 }
